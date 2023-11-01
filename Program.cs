692b9ad932c3d35d75ce4272b94e86989c31e1e7
Тим Гусь <guskov.tima@rambler.ru>
2023-11-01 10:16:28 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+
+namespace CsProfHomeWork7
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MeasureExecutionTime(".");
+        }
+        public static int CountSpacesInFile(string filePath)
+        {
+            var content = File.ReadAllText(filePath);
+            return content.Length - content.Replace(" ", "").Length;
+        }
+
+        public static int CountSpacesInDirectory(string directoryPath)
+        {
+            var files = Directory.GetFiles(directoryPath, "*.txt");
+            var tasks = files.Select(filePath => Task.Run(() => CountSpacesInFile(filePath))).ToArray();
+
+            Task.WaitAll(tasks);
+            return tasks.Sum(t => t.Result);
+        }
+
+        public static void MeasureExecutionTime(string directoryPath)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            var totalSpaces = CountSpacesInDirectory(directoryPath);
+
+            stopwatch.Stop();
+            Console.WriteLine($"Total spaces: {totalSpaces}. Time taken: {stopwatch.ElapsedMilliseconds} ms.");
+        }
+    }
+}
